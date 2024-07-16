ed5fc0e236ef099ed43738d9927dd96300a159d7
camdentadhg <camdent@gmail.com>
2024-07-16 14:54:25 -0500

    unroll completed

--- snack-or-booze/src/App.js
+++ snack-or-booze/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Home from "./Home";
 import SnackOrBoozeApi from "./Api";
 import NavBar from "./NavBar";
-import { Route, Switch } from "react-router-dom";
+import { Route, Router } from "react-router-dom";
 import Menu from "./FoodMenu";
 import Snack from "./FoodItem";
 
@@ -30,7 +30,7 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <main>
-          <Switch>
+          <Router>
             <Route exact path="/">
               <Home snacks={snacks} />
             </Route>
@@ -43,7 +43,7 @@ function App() {
             <Route>
               <p>Hmmm. I can't seem to find what you want.</p>
             </Route>
-          </Switch>
+          </Router>
         </main>
       </BrowserRouter>
     </div>
@@ -52,21 +52,19 @@ function App() {
 
 export default App;
 
-//create unroll function 
-  //create unroll function
-  //run tests
-  //refactor unroll function
-  //run tests
-  //the unroll script works as expected
-
 //create Snack or Booze website
+  //look through current code
+  //run program
   //change home page to show # of food items and # of drink choices
   //abstract the FoodMenu and FoodItem components for use with Drinks
   //add Drinks link to the navbar
   //add DrinksMenu component that matches the FoodMenu component, which teach drink choice being a link to details about the drink
   //add a page that lets users add either a drink or a snack
   //Refactor app using good design
+    //look for bad variable names
+    //look for AJAX calls in components
   //Write tests
+  //document code
   //handle not-found pages
   //the homepage shows the number of food items and drink choices
   //the navbar has a Drinks link leading to a page where the drinks are listed
@@ -76,7 +74,7 @@ export default App;
   //the app include tests
   //a message appears for not-found pages instead of an error
   //for non-existant drinks or foods, the app redirects to the respective listing page
-  //**the app allows the user to create custom cocktails and ingredients
+  //the app allows the user to create custom cocktails and ingredients
   //the app is bug free
   //an understanding of abstracting React components to reduce code duplication is demonstrated
   //an understanding of writing efficient React code is demonstrated
